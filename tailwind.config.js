1b107c79b0470b6e03d4cc4f5215100b47a7e520
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-08-20 13:58:12 +0700

    set config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color1: "#BD0707",
+        color2: "#FFFEFE",
+        color3: "#7E8CAC",
+        color4: "#2C273F",
+        color5: "#F6DADA",
+        color6: "#974A4A",
+      },
+    },
   },
   plugins: [],
 };
