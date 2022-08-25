51889c1506c327770a5ebae3cbd4b0e23ad39c85
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-08-25 09:53:40 +0700

    set confiq jest

--- src/components/organisms/Banner/index.js
+++ src/components/organisms/Banner/index.js
@@ -4,7 +4,7 @@ import { Jumbotron } from "../../../assets";
 const Banner = () => {
   return (
     <div className="flex justify-center">
-      <div className="xl:w-[958px] md:w-[650px] lg:w-[750px] h-auto bg-[#BD0707] sm:w-[550px]  rounded-[10px] text-white absolute top-[180px] ">
+      <div className="xl:w-[958px] md:w-[650px] lg:w-[750px] h-auto bg-color1 sm:w-[550px]  rounded-[10px] text-white absolute top-[180px] ">
         <div className="p-[57px]">
           <h1 className="text-[60px] font-black mb-4">WAYSBUCKS</h1>
           <p className="mb-7 text-2xl font-light">
