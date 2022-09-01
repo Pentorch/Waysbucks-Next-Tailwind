b59907d5a5ddf6b4c599f09fd46980e5fe12ccc1
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-09-01 11:50:37 +0700

    component -toppings

--- src/components/atoms/Icons/index.js
+++ src/components/atoms/Icons/index.js
@@ -1,13 +1,15 @@
 import User from "./User";
 import Logout from "./Logout";
 import Product from "./Product";
+import Toppings from "./Toppings";
 
 const Icons = {
   User,
   Logout,
   Product,
+  Toppings,
 };
 
 export default Icons;
 
-export { User, Logout, Product };
+export { User, Logout, Product, Toppings };
