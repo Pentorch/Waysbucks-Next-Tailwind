bf1f0730bdc65cfe682a9af409546f6c0ef44361
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-09-07 09:45:03 +0700

    update navbar

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import slice1 from "./images/slice1.svg";
 import slice2 from "./images/slice2.svg";
 import slice3 from "./images/slice3.svg";
 import slice4 from "./images/slice4.svg";
+import WaysBucks from "./images/WaysBucks.svg";
 
-export { Jumbotron, slice1, slice2, slice3, slice4 };
+export { Jumbotron, slice1, slice2, slice3, slice4, WaysBucks };
--- src/components/molecules/Navbar/index.js
+++ src/components/molecules/Navbar/index.js
@@ -1,55 +1,109 @@
-import React, { useState } from "react";
+import Images from "next/image";
+import { useState } from "react";
+import { WaysBucks } from "src/assets";
 import Button from "src/components/atoms/Button";
 
-const Navbar = () => {
-  let Links = [
-    { name: "HOME", link: "/" },
-    { name: "SERVICE", link: "/" },
-    { name: "ABOUT", link: "/" },
-    { name: "BLOG'S", link: "/" },
-    { name: "CONTACT", link: "/" },
-  ];
-  let [open, setOpen] = useState(false);
+export default function Navbar() {
+  const [navbar, setNavbar] = useState(false);
+
   return (
-    <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
-        <div
-          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
-      text-gray-800"
-        >
-          <span className="text-3xl text-indigo-600 mr-1 pt-2">
-            <ion-icon name="logo-ionic"></ion-icon>
-          </span>
-          Designer
-        </div>
-
-        <div
-          onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-        >
-          <ion-icon name={open ? "close" : "menu"}></ion-icon>
-        </div>
-
-        <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-20 " : "top-[-490px]"
-          }`}
-        >
-          {Links.map((link) => (
-            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
-              <a
-                href={link.link}
-                className="text-gray-800 hover:text-gray-400 duration-500"
+    <nav className="w-full shadow">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a href="javascript:void(0)">
+              <Images src={WaysBucks} />
+            </a>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
               >
-                {link.name}
-              </a>
-            </li>
-          ))}
-          <Button className="bg-color1 text-white">Get Started</Button>
-        </ul>
-      </div>
-    </div>
-  );
-};
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-black"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-black"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-black hover:text-indigo-200">
+                <a href="javascript:void(0)">Home</a>
+              </li>
+              <li className="text-black hover:text-indigo-200">
+                <a href="javascript:void(0)">Blog</a>
+              </li>
+              <li className="text-black hover:text-indigo-200">
+                <a href="javascript:void(0)">About US</a>
+              </li>
+              <li className="text-black hover:text-indigo-200">
+                <a href="javascript:void(0)">Contact US</a>
+              </li>
+            </ul>
 
-export default Navbar;
+            <div className="mt-3 space-y-2 lg:hidden md:inline-block">
+              <a
+                href="javascript:void(0)"
+                className="inline-block w-full px-4 py-2 text-center text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
+              >
+                Sign in
+              </a>
+              <Button
+                href="javascript:void(0)"
+                className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+              >
+                Sign up
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="hidden space-x-2 md:inline-block">
+          <a
+            href="javascript:void(0)"
+            className="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
+          >
+            Sign in
+          </a>
+          <a
+            href="javascript:void(0)"
+            className="px-4 py-2 text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+          >
+            Sign up
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
